efdbfa0726ba3c8873e245febbbca3d42ae24bb6
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-06 22:41:51 +0400

    description create

---
+++ src/components/description/description.tsx
@@ -0,0 +1,43 @@
+import './description.scss'
+
+export const Description = () => {
+
+    return <section className="description">
+        <article className="desctiption-wrapper">
+            <h2 className='description__title'>Award winning</h2>
+            <h2 className='description__title'>cafe and bar.</h2>
+        </article>
+        <article className="desctiption-wrapper">
+            <p className='description__content'>
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do
+            </p>
+            <p className='description__content'>
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim
+            </p>
+            <p className='description__content'>
+                ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut
+            </p>
+            <p className='description__content'>
+                aliquip 
+                ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <p className='description__content'>
+                
+            </p>
+            <p className='description__content'>
+                reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla
+            </p>
+            <p className='description__content'>
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            </p>
+            <p className='description__content'> 
+                culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </article>
+    </section>
+}
